4ac59d1ac4535f4b0cccb2311fb0041ce10091d9
BlackRavenMomon <ashishbelel70@gmail.com>
2026-02-19 19:16:31 +0530

    Printing title

--- src/main/java/com/elpais/ElPaisTest.java
+++ src/main/java/com/elpais/ElPaisTest.java
@@ -1,10 +1,25 @@
 package com.elpais;
 
-/**
- * Hello world!
- */
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class ElPaisTest {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        WebDriverManager.chromedriver().setup(); //going with chrome first
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://elpais.com/");
+
+        System.out.println("Title of page: " + driver.getTitle());
+
+        driver.quit();
     }
 }
